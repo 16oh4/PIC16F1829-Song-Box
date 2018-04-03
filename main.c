65ead04411957fe23671404996c8ad8394b54cea
brunogracia <37783822+brunogracia@users.noreply.github.com>
2018-04-03 15:03:45 -0500

    New

--- main.c
+++ main.c
@@ -29,16 +29,94 @@
 #define G 170
 #define A 153
 #define B 136
+#define R 0
 #define C2 127
 #define x 14
 
+int songnum = 0;
 int song[x]={C, C, G, G, A, A, G, F, F, E, E, D, D, C}; //insert notes of song in array
 int song2[x]={
     D,D,D,D,
     F,F,F,F,
     G,G,G,G,
-    C,C};
-int length[x]={1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2}; //relative length of each note
+    C,C
+};
+int song3[x] = {
+    C,D,E,F,
+    G,A,B,C2,
+    C,D,E,F,
+    G,A
+};
+int song4[16] = {
+    G,G,D,R,
+    C,C,D,E,
+    D,D,F,R,
+    R,R,F,F
+};
+int length4[16] = {
+    1,1,2,2,
+    1,1,2,2,
+    2,2,1,1,
+    2,2,1,1
+};
+int song5[] = {
+    C,C,
+        D,E,
+    D,D,F,
+    
+    R,
+        D,E,
+    D,D,C,
+    
+    R,
+    
+        F,F,
+    G,G,D,
+    
+    C,
+        F,F,
+    G,G,D,
+    
+    R,
+    
+        D,D,
+    G,G,F,
+    
+    C,
+        D,D,
+    G,G,F,R
+};
+int length5[] = {
+    1,1,
+        2,2,
+    2,2,1,
+    
+    1,
+        
+        2,2,
+    2,2,1,
+    
+    1,
+    
+        2,2,
+    2,2,1,
+    
+    1,
+        2,2,
+    2,2,1,
+    
+    1,
+    
+        2,2,
+    2,2,1,
+    
+    1,
+    
+        2,2,
+    2,2,1,1
+};
+int length[x]={2, 2, 2, 2, 1, 1, 1, 1, 4, 4, 4, 4, 3, 3};
+int length3[x]={1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; //relative length of each note
 int i;
 
 //Global Variables 
@@ -49,7 +127,7 @@ void PWM_Initialize()
 {  
 //    APFCON1 = 0x01;  //Set PWM to alternative PIN RA5 (HELLO WORLD LED)
     APFCON1 = 0x00; //Set PWM to RC3
-    PR2 = 0xEE;  //PR2 value needed for 1000Hz frequency
+    PR2 = 0xFF;  //PR2 value needed for 1000Hz frequency
     CCP2CON = 0x0C; //Enable pwm mode pg.236
     //CCP2CON = 0b01001100;
     CCPR2L = 0x00;  //Set initial dutycycle to 0
@@ -80,33 +158,48 @@ void checkButton()
 {
     if(!RB5)
     {
-        TRISA5 = ~TRISA5; //Toggle using bitwise not
+        //TRISA5 = ~TRISA5; //Toggle using bitwise not
+        songnum++;
     }
 }
 void playSong(int beat[], int time[], int notes)
 {
-    for(int i = 0; i < notes; i++)
+    for(i = 0; i < notes; i++)
     {
         PWM_Duty(beat[i]); //play note
-        T2CON = 0b00000100;
-        __delay_ms(100);//for length specified
+        //ROSEMARY UART HERE
         
+        //T2CON = 0b00000100;
+        for(int j=0; j < time[i]; j++)
+        {
+            __delay_ms(25);//for length specified
+        }
 //        PIR1 &= 0xFF;
-        T2CON = 0b00000100;
+        //T2CON = 0b00000100;
         PWM_Duty(0); //short silence in between notes
-        __delay_ms(50);
+        __delay_ms(5);
         TMR2 = 0x0;
     }
 }
+
+
 void main()
 {
     TRISC3 = 0;
     PWM_Initialize();
-    PWM_Duty(500); //volume
+    PWM_Duty(1000); //volume
+    
     do
     {
+//        
+        //CARLOS LCD HERE
+        playSong(song5, length5, 38);
         
-        playSong(song2, length, x);
+        
+//        if(songnum=4)
+//        {
+//            i=1;
+//        }
 
     }
     while(1);
